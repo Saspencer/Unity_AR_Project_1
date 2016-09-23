8b49eac38f051d4a4b1eb4c573da94a840cf63a1
Saspencer <andrey.spencer@yahoo.com>
2016-09-22 21:18:44 -0700

    Andrey Spencer
    Andrey Spencer

---
+++ AR Maze Game 2/Assets/Scripts/SphereMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMovement : MonoBehaviour 
+{
+    public GameObject plane;
+    public GameObject spawnPoint;
+	
+	void Start () 
+    {
+        //transform.position = spawnPoint.transform.position;
+	}
+	
+	
+	void Update () 
+    {
+	    if(transform.position.y < plane.transform.position.y - 10)
+        {
+            transform.position = spawnPoint.transform.position;
+        }
+	}
+}
